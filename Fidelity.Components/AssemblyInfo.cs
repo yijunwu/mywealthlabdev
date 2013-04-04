cd1cbecd41bc5582f61f082c01aa2a30139b242f
wuyijun <wuyijun1129@hotmail.com>
2013-04-04 10:43:36 +0800

    Initially adding new files

---
+++ Fidelity.Components/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// Assembly Fidelity.Components, Version 1.0.0.0
+
+[assembly: System.Runtime.Versioning.TargetFramework(".NETFramework,Version=v4.0", FrameworkDisplayName=".NET Framework 4")]
+[assembly: System.Runtime.InteropServices.Guid("c7208dda-2a27-478d-8fa6-5ca99e7ec679")]
+[assembly: System.Reflection.AssemblyProduct("Fidelity.Components")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Reflection.AssemblyCompany("Fidelity Investments")]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Reflection.AssemblyTitle("Fidelity.Components")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9  2007-2010 Fidelity Investments")]
+
