d8338ce5852967c7942a39e71511b8af2a6fd8db
wuyijun <wuyijun1129@hotmail.com>
2013-04-04 10:44:31 +0800

    Initially adding new files

---
+++ SyntaxSettingsConverter/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// Assembly SyntaxSettingsConverter, Version 1.0.0.0
+
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("QWhale")]
+[assembly: System.Reflection.AssemblyProduct("SyntaxSettingsConverter")]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Reflection.AssemblyTitle("SyntaxSettingsConverter")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 QWhale 2008")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Runtime.InteropServices.Guid("8f2e73f4-3627-4d1d-9e56-6875193a017b")]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+
